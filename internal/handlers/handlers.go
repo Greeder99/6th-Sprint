2a4df70a46259b4e929d7f9bdfce783843a183c9
Гуров Иван <ivangurov.999@mail.ru>
2025-05-07 16:29:11 +0300

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,68 @@
 package handlers
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HtmlHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("../index.html")
+	if err != nil {
+		http.Error(w, "I can't read Html-file", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	file, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	converted := service.Conversation(string(data))
+
+	ext := filepath.Ext(header.Filename)
+	filename := strings.ReplaceAll(time.Now().UTC().String(), " ", "_")
+	filename = strings.ReplaceAll(filename, ":", "-") + ext
+	localFile, err := os.OpenFile(filepath.Join("..", filename), os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	defer localFile.Close()
+
+	if _, err = localFile.Write([]byte(converted)); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	if _, err := w.Write([]byte(converted)); err != nil {
+		log.Printf("Error sending response: %v", err)
+	}
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Conversation(stringOrMorse string) string {
+	var formattext bool
+	for _, i := range strings.ToLower(stringOrMorse) {
+		// Провожу проверку на то, есть ли в переменной какие то другие символы кроме - и . Проверка по русскому алфавиту потому что в пакете morse мы работает именно с русским алфавитом
+		if (i >= 1072 && i <= 1103) || i == 1105 || i == 34 || (i >= 39 && i <= 41) || i == 43 || i == 44 || (i >= 47 && i <= 58) || i == 61 || i == 63 || i == 64 {
+			formattext = true
+		} else {
+			break
+		}
+	}
+	if formattext {
+		return morse.ToMorse(stringOrMorse)
+	} else {
+		return morse.ToText(stringOrMorse)
+	}
+}
